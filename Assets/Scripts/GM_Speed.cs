293405c6a909de3f906bd994fa8b6745b6ceba0d
Leon Hogan <73083286+ElwoodHogan@users.noreply.github.com>
2022-03-29 23:11:12 -0500

    minor update

---
+++ Assets/Scripts/GM_Speed.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Gun Mods/Speed")]
+public class GM_Speed : GunMod
+{
+    protected override void TransmuteBlock(Collider shotBlock)
+    {
+        shotBlock.gameObject.AddComponent<BM_Speed>();
+    }
+}
