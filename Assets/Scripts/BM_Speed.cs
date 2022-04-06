cc511889536252528f0884c0adf0024b7c056657
Leon Hogan <73083286+ElwoodHogan@users.noreply.github.com>
2022-04-05 19:59:07 -0500

    Aded speed

---
+++ Assets/Scripts/BM_Speed.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BM_Speed : BlockMod
+{
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<RigidbodyController>().Speed =
+                25f;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<RigidbodyController>().Speed =
+                5f;
+        }
+    }
+}
