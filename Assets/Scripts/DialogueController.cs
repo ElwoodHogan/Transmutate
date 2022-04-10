af36507afcb191c0d212641345d8d68a0d7b1b7e
Ronald Riggles <rlr0017@auburn.edu>
2022-04-10 17:04:27 -0500

    Updated DialogueController Functionality

--- Assets/Scripts/DialogueController.cs
+++ Assets/Scripts/DialogueController.cs
@@ -1,33 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
 
 public class DialogueController : MonoBehaviour
 {
-    private Queue<string> DialogueQueue = new Queue<string>();
-    private bool DialogueActive = false;
+    public Text textBox;
+    public float textInterval; // The speed at which text will type into the text box.
+
+    private Queue<string> dialogueQueue = new Queue<string>();
+    private StringBuilder stringBuf = new StringBuilder("");
+    private bool dialogueActive = false;
+    private static string[] alphaCodes = {
+        "<color=#ffffff40>",
+        "<color=#ffffff80>",
+        "<color=#ffffffbf>",
+        "<color=#ffffffff>",
+    };
+
+    // Places a dialogue sequence associated with the given reference string into the queue for playback.
+    public void EnqueueDialogue(string dialogueRef)
+    {
+        dialogueQueue.Enqueue(dialogueRef);
+    }
 
     void Update()
     {
-        if (!DialogueActive)
+        if (!dialogueActive)
         {
-            if (DialogueQueue.Count > 0)
+            if (dialogueQueue.Count > 0)
             {
-                DialogueActive = true;
-                StartCoroutine(PlayDialogue(DialogueQueue.Dequeue()));
+                dialogueActive = true;
+                StartCoroutine(PlayDialogue(dialogueQueue.Dequeue()));
             }
         }
     }
 
     private IEnumerator PlayDialogue(string dialogueRef)
     {
-        // Placeholder
-        DialogueActive = false;
-        return null;
+        foreach (string dialogueLine in GetDialogue(dialogueRef))
+        {
+            for (int i = 0; i < dialogueLine.Length + alphaCodes.Length; i++)
+            {
+                UpdateBufAlpha();
+                if (i < dialogueLine.Length)
+                {
+                    AppendBufChar(dialogueLine[i]);
+                }
+                textBox.text = stringBuf.ToString();
+                yield return new WaitForSeconds(textInterval);
+            }
+            stringBuf.Clear();
+        }
+        dialogueActive = false;
     }
 
-    public void EnqueueDialogue(string dialogueRef)
+    // Increments the alpha (transparency) of each character in the string buffer by one level.
+    private void UpdateBufAlpha()
     {
-        DialogueQueue.Enqueue(dialogueRef);
+        for (int i = 0; i < alphaCodes.Length - 1; i++)
+        {
+            stringBuf.Replace(alphaCodes[i], alphaCodes[i + 1]);
+        }
+    }
+
+    // Appends a new character to the string buffer, with its alpha (transparency) set at the lowest level.
+    private void AppendBufChar(char c)
+    {
+        stringBuf.AppendFormat("{0}{1}</color>", alphaCodes[0], c);
+    }
+
+    // Returns a list of dialogue lines associated with the given reference string.
+    private List<string> GetDialogue(string dialogueRef)
+    {
+        return null; // Placeholder
     }
 }
