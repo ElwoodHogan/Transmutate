8d3b7a58806ad2035310155b5afa5a2b569a44d6
Ronald Riggles <rlr0017@auburn.edu>
2022-04-18 21:26:48 -0500

    Renamed script to match conventionality.

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoomChange : MonoBehaviour
+public class DialogueTrigger : MonoBehaviour
 {
     public DialogueController dialogueController;
     public string dialogueRef;
